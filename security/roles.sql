b83c9e9cad6ac438f62d225e4a5721accec02dae
Yzma01 <marchena.isma@gmail.com>
2025-10-10 19:20:38 -0600

    sysdbas users

--- security/roles.sql
+++ security/roles.sql
@@ -1,4 +1,28 @@
+create role basic;
+grant CONNECT to basic;
+grant CREATE SESSION to basic;
+
 create role manager;
+grant basic to manager;
+grant CREATE TABLE to manager;
+grant CREATE VIEW to manager;
+grant CREATE PROCEDURE to manager;
+grant CREATE SEQUENCE to manager;
+grant CREATE TRIGGER to manager;
+grant CREATE TYPE to manager;
+
 create role agronomist;
+grant basic to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE on SYS.TBL_AGRONOMISTS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_FIELDS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_CROPS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_PESTICIDES to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_FERTILIZERS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_TREATMENTS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_HARVESTS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_IRRIGATIONS to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_WEATHER to agronomist;
+grant SELECT, INSERT, UPDATE, DELETE  on SYS.TBL_SOIL_ANALYSES to agronomist;
+
 create role technical;
 create role secretary;
---
+++ security/users.sql
@@ -0,0 +1,12 @@
+-- SYSBDA's
+create user reds IDENTIFIED by reds;
+grant sys to reds;
+grant basic to reds;
+
+create user fabiux IDENTIFIED by fabiux;
+grant sys to fabiux;
+grant basic to fabiux;
+
+create user yzma IDENTIFIED by yzma;
+grant sys to yzma;
+grant basic to yzma;
